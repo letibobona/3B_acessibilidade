d3d0678abc43754c6407479ec8c6bfd2b409c899
Letícia Vieira <148462647+letibobona@users.noreply.github.com>
2025-04-15 10:54:21 -0300

    diminui

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 const aumentaFonteBotao = document.getElementById("aumentar-fonte");
-const diminuiFonteBotao = document.getElementById("diminui-fonte");
+const diminuiFonteBotao = document.getElementById("diminuir-fonte");
 
 let tamanhoAtualFonte = 1;
 
