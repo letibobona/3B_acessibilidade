7fdbfff3b1f10db4d6ea11962be6fce3b0710c26
Letícia Vieira <148462647+letibobona@users.noreply.github.com>
2025-04-15 10:52:21 -0300

    diminui

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ aumentaFonteBotao.addEventListener("click", function(){
 tamanhoAtualFonte += 0.1;
 document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
-
-
-
-
-
-
 })
+
+diminuiFonteBotao.addEventListener("click", function(){
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    
+    })
 
 
 })
